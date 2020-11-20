88db9367b9e339cd7815ed1b8946b594a4de2bf1
Jack-Pults <jack.pults@gmail.com>
2020-11-20 15:20:26 -0600

    Updates

---
+++ precache-manifest.8fa3aabef29a1daa1f0283c7cce9a622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd087bc8ea772ad5ac6a41f87d22538",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b18154b38011e52c130",
+    "url": "/static/css/main.5692ce97.chunk.css"
+  },
+  {
+    "revision": "f7f9ea4b55d2dde13f08",
+    "url": "/static/js/2.758e9b06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.758e9b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b18154b38011e52c130",
+    "url": "/static/js/main.b35f22b9.chunk.js"
+  },
+  {
+    "revision": "2d9417c07231f4c71ce2",
+    "url": "/static/js/runtime-main.c08ed251.js"
+  }
+]);
